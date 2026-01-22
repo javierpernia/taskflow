6659818891050babb99fc81e7066e4aee3dbd253
Javier Pernía <javier.perniac@gmail.com>
2026-01-22 16:05:59 -0400

    creacion de useTasksReducer

--- types/index.ts
+++ types/index.ts
@@ -3,4 +3,18 @@ interface User {
   name: string;
 };
 
-export type { User };
+// TaskStatus: define los estados posibles de una tarea
+type TaskStatus = 'pending' | 'inProgress' | 'completed';
+
+interface Task {
+  id: number;
+  title: string;
+  status: TaskStatus;
+}
+
+type TaskAction =
+  | { type: 'ADD_TASK'; payload: string }
+  | { type: 'MOVE_TASK'; payload: number }
+  | { type: 'DELETE_TASK'; payload: number };
+
+export type { User, Task, TaskAction, TaskStatus };
